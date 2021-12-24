6b07b51f7317a93f5d1a04bdee90650032f612a6
vivekpatel17 <vivekpatelk0@gamil.com>
2021-12-24 13:03:10 +0530

    .

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import classes from "./Header.module.css";
 export default function Header() {
   return (
     <header className={classes.header}>
-      <h1>Keeper</h1>
+      <h1>T notes</h1>
     </header>
   );
 };
